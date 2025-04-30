fc553a3d7dba7029f3f867859c3542c1ed1b489b
Johnny Faris <jkfaris@gmail.com>
2025-04-30 09:45:31 -0700

    added error handler for not allowed methods

--- src/pastes/pastes.router.js
+++ src/pastes/pastes.router.js
@@ -1,8 +1,20 @@
 const router = require("express").Router(); //creates a new instance of an Express router.
 const controller = require("./pastes.controller"); //imports the /pastes controller that you created earlier.
+const methodNotAllowed = require("../errors/methodNotAllowed"); //methodNotAllowed added in
 
-router.route("/").get(controller.list).post(controller.create); //using route() allows you to write the path once and then link multiple route handlers to that path.
+router
+.route("/")
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed);
+//using route() allows you to write the path once and then link multiple route handlers to that path.
 //get(controller.list) uses the list() route handler defined in the controller for GET requests to /
-router.route("/:pasteId").get(controller.read).put(controller.update).delete(controller.delete);
+
+router
+.route("/:pasteId")
+.get(controller.read)
+.put(controller.update)
+.delete(controller.delete)
+.all(methodNotAllowed);
 
 module.exports = router; //exports the router for use in app.js.
--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
-
 const controller = require("./users.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/:userId").get(controller.read);
+router
+    .route("/:userId")
+    .get(controller.read)
+    .all(methodNotAllowed);
 
-router.route("/").get(controller.list);
+router
+    .route("/")
+    .get(controller.list)
+    .all(methodNotAllowed);
 
 module.exports = router;
