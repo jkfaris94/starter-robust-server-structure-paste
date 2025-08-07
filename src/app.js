2dac0d96ed0a62e53d7b38c5844ad6850e097bc4
Johnny <jkfaris@gmail.com>
2025-08-07 10:36:38 -0700

    added errors

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ app.use("/pastes/:pasteId", (req, res, next) => {
   if (foundPaste) {
     res.json({ data: foundPaste });
   } else {
-    next(`Paste id not found: ${pasteId}`);
+    next({ status: 404, message: `Paste id not found: ${pasteId}` });
   }
 });
 
@@ -23,14 +23,29 @@ app.get("/pastes", (req, res) => {
   res.json({ data: pastes });
 });
 
+// New middleware function to validate the request body
+function bodyHasTextProperty(req, res, next) {
+  const { data: { text } = {} } = req.body;
+  if (text) {
+    return next(); // Call `next()` without an error message if the result exists
+  }
+  next({
+    status: 400,
+    message: "A 'text' property is required."
+  });
+}
+
 // Variable to hold the next ID
 // Because some IDs may already be used, find the largest assigned ID
 let lastPasteId = pastes.reduce((maxId, paste) => Math.max(maxId, paste.id), 0);
 
-app.post("/pastes", (req, res, next) => {
+app.post(
+  "/pastes", 
+  bodyHasTextProperty,
+  (req, res, next) => {
   const { data: { name, syntax, exposure, expiration, text, user_id } = {} } =
     req.body;
-  if (text) {
+  {
     const newPaste = {
       id: ++lastPasteId, // Increment last ID, then assign as the current ID
       name,
@@ -42,10 +57,7 @@ app.post("/pastes", (req, res, next) => {
     };
     pastes.push(newPaste);
     res.status(202).json({ data: newPaste });
-  } else {
-    res.sendStatus(400);
-  }
-
+  } 
 });
 
 // Not found handler
@@ -54,9 +66,10 @@ app.use((request, response, next) => {
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "something went wrongz!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
